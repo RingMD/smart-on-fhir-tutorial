8cfcd8f0329d0548ba6f5000c9a6f15f03d2736e
kennyki <knyki.12@gmail.com>
2023-08-16 16:53:55 +0800

    need to specify max start

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -8,8 +8,11 @@
     }
 
     async function readSlots (client) {
-      const now = (new Date()).toISOString()
-      const slots = await client.request(`Slot/?_id=${client.user.id}&start=ge${now}`)
+      const now = new Date()
+      const later = (new Date()).setDate(now.getDate() + 30)
+      const min = now.toISOString()
+      const max = later.toISOString()
+      const slots = await client.request(`Slot/?_id=${client.user.id}&start=ge${min}&start=lt${max}`)
 
       console.log(slots)
     }
