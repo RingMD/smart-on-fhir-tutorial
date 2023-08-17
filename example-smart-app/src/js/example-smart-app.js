871701e0be0a7b51f616c0fa31ad40b969d84535
kennyki <knyki.12@gmail.com>
2023-08-17 16:14:27 +0800

    use vue

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -1,139 +1,112 @@
-(function(window){
-  window.extractData = function() {
-    var deferred = $.Deferred();
+(function (window) {
 
-    function onError() {
-      console.log('Loading error', arguments);
-      deferred.reject();
-    }
+  const { createApp, reactive, ref } = Vue
 
-    // http://docs.smarthealthit.org/client-js/fhirjs-equivalents
-    function getQuery (params) {
-      const query = new URLSearchParams()
+  async function setup () {
+    const display = reactive({})
+    const error = ref(null)
+    const isLoading = ref(true)
 
-      params.forEach(param => {
-        query.append(param.key, param.value)
-      })
+    try {
+      const client = await FHIR.oauth2.ready()
 
-      return query
-    }
-
-    async function readSlots (client) {
-      const now = new Date()
-      const later = new Date()
-      later.setDate(now.getDate() + 30)
-      const min = now.toISOString()
-      const max = later.toISOString()
-      // get from the sandbox test data - "Video Visit"
-      // https://docs.google.com/document/d/10RnVyF1etl_17pyCyK96tyhUWRbrTyEcqpwzW-Z-Ybs/edit#heading=h.78lvm8ihmcyu
-      // for production, service types must be provided by the implementation team
-      const serviceType = 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/14249|2572307911'
-      const query = getQuery([
-        { key: 'schedule.actor', value: client.user.fhirUser },
-        { key: 'service-type', value: serviceType },
-        { key: 'start', value: `ge${min}` },
-        { key: 'start', value: `lt${max}` }
-      ])
-      const slots = await client.request(`Slot/?${query}`)
-
-      console.log(slots)
-    }
-
-    async function onReady(client)  {
       console.log('Practitioner resource identity: ' + client.user.fhirUser)
       console.log('Patient resource identity: ' + client.getState('serverUrl') + '/Patient/' + client.patient.id)
 
-      readSlots(client)
+      const observationQuery = getQuery([
+        { key: 'patient', value: client.patient.id },
+        { key: 'code', value: [
+                                'http://loinc.org|8302-2',
+                                'http://loinc.org|8462-4',
+                                'http://loinc.org|8480-6',
+                                'http://loinc.org|2085-9',
+                                'http://loinc.org|2089-1',
+                                'http://loinc.org|55284-4'
+                              ].join(',') }
+      ])
+      const [patient, observations, slots] = await Promise.all([
+        client.patient.read(),
+        client.request(`Observation?${observationQuery}`, {
+          pageLimit: 0,
+          flat: true
+        }),
+        readSlots(client)
+      ])
 
-      if (client.hasOwnProperty('patient')) {
-        try {
-          const observationQuery = getQuery([
-            { key: 'patient', value: client.patient.id },
-            { key: 'code', value: [
-                                    'http://loinc.org|8302-2',
-                                    'http://loinc.org|8462-4',
-                                    'http://loinc.org|8480-6',
-                                    'http://loinc.org|2085-9',
-                                    'http://loinc.org|2089-1',
-                                    'http://loinc.org|55284-4'
-                                  ].join(',') }
-          ])
-          const [patient, observations] = await Promise.all([
-            client.patient.read(),
-            client.request(`Observation?${observationQuery}`, {
-              pageLimit: 0,
-              flat: true
-            })
-          ])
+      display.value = displayPatient(client, patient, observations)
+    } catch (ex) {
+      error.value = ex
+    } finally {
+      isLoading.value = false
+    }
 
-          console.log(patient)
+    return {
+      display,
+      error,
+      isLoading
+    }
+  }
 
-          var byCodes = client.byCodes(observations, 'code');
-          var gender = patient.gender;
+  // http://docs.smarthealthit.org/client-js/fhirjs-equivalents
+  function getQuery (params) {
+    const query = new URLSearchParams()
 
-          var fname = '';
-          var lname = '';
+    params.forEach(param => {
+      query.append(param.key, param.value)
+    })
 
-          if (typeof patient.name[0] !== 'undefined') {
-            fname = patient.name[0].given.join(' ');
-            // https://github.com/cerner/smart-on-fhir-tutorial/pull/213
-            if (Array.isArray(patient.name[0].family)) {
-              lname = patient.name[0].family.join(' ');
-            } else {
-              lname = patient.name[0].family;
-            }
-          }
+    return query
+  }
 
-          var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          var hdl = byCodes('2085-9');
-          var ldl = byCodes('2089-1');
+  async function readSlots (client) {
+    const now = new Date()
+    const later = new Date()
+    later.setDate(now.getDate() + 30)
+    const min = now.toISOString()
+    const max = later.toISOString()
+    // get from the sandbox test data - "Video Visit"
+    // https://docs.google.com/document/d/10RnVyF1etl_17pyCyK96tyhUWRbrTyEcqpwzW-Z-Ybs/edit#heading=h.78lvm8ihmcyu
+    // for production, service types must be provided by the implementation team
+    const serviceType = 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/14249|2572307911'
+    const query = getQuery([
+      { key: 'schedule.actor', value: client.user.fhirUser },
+      { key: 'service-type', value: serviceType },
+      { key: 'start', value: `ge${min}` },
+      { key: 'start', value: `lt${max}` }
+    ])
+    const slots = await client.request(`Slot/?${query}`)
 
-          var p = defaultPatient();
-          p.birthdate = patient.birthDate;
-          p.gender = gender;
-          p.fname = fname;
-          p.lname = lname;
-          p.height = getQuantityValueAndUnit(height[0]);
+    console.log(slots)
+  }
 
-          if (typeof systolicbp != 'undefined')  {
-            p.systolicbp = systolicbp;
-          }
+  function displayPatient (client, patient, observations) {
+    const byCodes = client.byCodes(observations, 'code')
+    const display = {
+      firstName: '-',
+      lastName: '-',
+      gender: patient.gender,
+      birthDate: patient.birthDate,
+      height: getQuantityValueAndUnit(byCodes('8302-2')[0]),
+      systolicbp: getBloodPressureValue(byCodes('55284-4'),'8480-6'),
+      diastolicbp: getBloodPressureValue(byCodes('55284-4'),'8462-4'),
+      hdl: getQuantityValueAndUnit(byCodes('2085-9')[0]),
+      ldl: getQuantityValueAndUnit(byCodes('2089-1')[0])
+    }
 
-          if (typeof diastolicbp != 'undefined') {
-            p.diastolicbp = diastolicbp;
-          }
+    const name = patient.name[0]
 
-          p.hdl = getQuantityValueAndUnit(hdl[0]);
-          p.ldl = getQuantityValueAndUnit(ldl[0]);
+    if (name) {
+      display.firstName = name.given.join(' ')
 
-          deferred.resolve(p)
-        } catch (error) {
-          onError(error)
-        }
+      // https://github.com/cerner/smart-on-fhir-tutorial/pull/213
+      if (Array.isArray(name.family)) {
+        display.lastName = name.family.join(' ')
       } else {
-        onError();
+        display.lastName = name.family
       }
     }
 
-    FHIR.oauth2.ready().then(onReady).catch(onError);
-    return deferred.promise();
-
-  };
-
-  function defaultPatient(){
-    return {
-      fname: {value: ''},
-      lname: {value: ''},
-      gender: {value: ''},
-      birthdate: {value: ''},
-      height: {value: ''},
-      systolicbp: {value: ''},
-      diastolicbp: {value: ''},
-      ldl: {value: ''},
-      hdl: {value: ''},
-    };
+    return display
   }
 
   function getBloodPressureValue(BPObservations, typeOfPressure) {
@@ -160,22 +133,14 @@
         typeof ob.valueQuantity.unit != 'undefined') {
           return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
     } else {
-      return undefined;
+      return '-';
     }
   }
 
-  window.drawVisualization = function(p) {
-    $('#holder').show();
-    $('#loading').hide();
-    $('#fname').html(p.fname);
-    $('#lname').html(p.lname);
-    $('#gender').html(p.gender);
-    $('#birthdate').html(p.birthdate);
-    $('#height').html(p.height);
-    $('#systolicbp').html(p.systolicbp);
-    $('#diastolicbp').html(p.diastolicbp);
-    $('#ldl').html(p.ldl);
-    $('#hdl').html(p.hdl);
-  };
+  window.rmd = {
+    init () {
+      createApp({ setup }).mount('#app')
+    }
+  }
 
 })(window);
