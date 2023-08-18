2592e3a6a4faf5edf2aa071271dd5bf3956786ce
kennyki <knyki.12@gmail.com>
2023-08-18 18:09:58 +0800

    oh well

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -102,7 +102,7 @@
           url: `Appointment/${appointment.id}`,
           method: 'PATCH',
           headers: {
-            'Content-Type': 'application/fhir+json'
+            'Content-Type': 'json-patch+json'
           },
           body: JSON.stringify([
             { op: 'replace', path: '/status', value: 'cancelled' },
